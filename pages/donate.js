efc35ad97de36526fe67f31deccb111440e3d87d
hobbleabbas <74548347+hobbleabbas@users.noreply.github.com>
2021-09-27 18:25:38 -0400

    Small iterator bug

--- pages/donate.js
+++ pages/donate.js
@@ -304,7 +304,7 @@ export default function Home() {
 									<p className="text-gray-500 text-sm">{cause.blurb}</p>
 									<p className = 'text-sm font-semibold mt-2'>Availiable in:{' '}
 										{cause.regions.map((region, regionIdx)=>(
-											<span className = 'font-normal'>{region.name} | </span>
+											<span key = {regionIdx} className = 'font-normal'>{region.name} | </span>
 										))}
 									</p>
 								</div>
