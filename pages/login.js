498bc39b576fabfdc745a1ecaa84b0c259027240
hobbleabbas <74548347+hobbleabbas@users.noreply.github.com>
2021-09-27 14:59:20 -0400

    Forgot password modal

--- pages/login.js
+++ pages/login.js
@@ -1,11 +1,16 @@
 import { supabase } from '../lib/supabaseClient'
-import { useState } from 'react'
+import { useState, useRef, Fragment } from 'react'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
+import { Dialog, Transition } from '@headlessui/react'
+import { ExclamationIcon, QuestionMarkCircleIcon } from '@heroicons/react/outline'
 
 export default function Register() {
 
 	const router = useRouter()
+
+	const [open, setOpen] = useState(false)
+	const cancelButtonRef = useRef(null)
 
 	const [loading, setLoading] = useState(false)
 	const [email, setEmail] = useState(null)
@@ -28,6 +33,24 @@ export default function Register() {
 		}
 
 		setLoading(false)
+	}
+
+	async function signInWithDiscord() {
+		const { user, session, error } = await supabase.auth.signIn({
+			provider: 'discord'
+		});
+	}
+
+	async function signInWithFacebook() {
+		const { user, session, error } = await supabase.auth.signIn({
+		  provider: 'facebook'
+		});
+	}
+
+	async function signInWithGoogle() {
+		const { user, session, error } = await supabase.auth.signIn({
+		  provider: 'google'
+		});
 	}
 
 	return (
@@ -95,11 +118,11 @@ export default function Register() {
 						</label>
 						</div>
 
-						{/* <div className="text-sm">
-						<a href="#" className="font-medium text-blue-600 hover:text-blue-500">
-							Forgot your password?
-						</a>
-						</div> */}
+						<div className="text-sm">
+							<a href = '#' onClick = {()=>{setOpen(true)}} className="font-medium text-blue-600 hover:text-blue-500">
+								Forgot your password?
+							</a>
+						</div>
 					</div>
 
 					<div>
@@ -129,15 +152,14 @@ export default function Register() {
 						<a
 							href="#"
 							className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+							onClick={signInWithFacebook}
 						>
 							<span className="sr-only">Sign in with Facebook</span>
-							<svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-							<path
-								fillRule="evenodd"
-								d="M20 10c0-5.523-4.477-10-10-10S0 4.477 0 10c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V10h2.54V7.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V10h2.773l-.443 2.89h-2.33v6.988C16.343 19.128 20 14.991 20 10z"
-								clipRule="evenodd"
+							<img
+								className="mx-auto h-6 w-auto"
+								src="/auth/facebook.png"
+								alt="Facebook Logo"
 							/>
-							</svg>
 						</a>
 						</div>
 
@@ -145,11 +167,14 @@ export default function Register() {
 						<a
 							href="#"
 							className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+							onClick={signInWithGoogle}
 						>
-							<span className="sr-only">Sign in with Twitter</span>
-							<svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-							<path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
-							</svg>
+							<span className="sr-only">Sign in with Google</span>
+							<img
+								className="mx-auto h-6 w-auto"
+								src="/auth/google.png"
+								alt="Google Logo"
+							/>
 						</a>
 						</div>
 
@@ -157,21 +182,111 @@ export default function Register() {
 						<a
 							href="#"
 							className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+							onClick={signInWithDiscord}
 						>
-							<span className="sr-only">Sign in with GitHub</span>
-							<svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-							<path
-								fillRule="evenodd"
-								d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z"
-								clipRule="evenodd"
+							<span className="sr-only">Sign in with Discord</span>
+							<img
+								className="mx-auto h-6 w-auto"
+								src="/auth/discord.png"
+								alt="Discord Logo"
 							/>
-							</svg>
 						</a>
 						</div>
 					</div>
 					</div>
 				</div>
 			</div>
+			<Transition.Root show={open} as={Fragment}>
+				<Dialog as="div" className="fixed z-10 inset-0 overflow-y-auto" initialFocus={cancelButtonRef} onClose={setOpen}>
+					<div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+					<Transition.Child
+						as={Fragment}
+						enter="ease-out duration-300"
+						enterFrom="opacity-0"
+						enterTo="opacity-100"
+						leave="ease-in duration-200"
+						leaveFrom="opacity-100"
+						leaveTo="opacity-0"
+					>
+						<Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+					</Transition.Child>
+
+					{/* This element is to trick the browser into centering the modal contents. */}
+					<span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
+						&#8203;
+					</span>
+					<Transition.Child
+						as={Fragment}
+						enter="ease-out duration-300"
+						enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+						enterTo="opacity-100 translate-y-0 sm:scale-100"
+						leave="ease-in duration-200"
+						leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+						leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+					>
+						<div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+						<div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+							<div className="sm:flex sm:items-start">
+							<div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-blue-100 sm:mx-0 sm:h-10 sm:w-10">
+								<QuestionMarkCircleIcon className="h-6 w-6 text-blue-600" aria-hidden="true" />
+							</div>
+							<div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+								<Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
+									Forgot Password
+								</Dialog.Title>
+								<div className="mt-2">
+
+								<p className="text-sm text-gray-500 mb-2">
+									If you forgot your password, enter your email below to get a password reset link. Click on the link and you will be able to enter a new password.
+								</p>
+
+								<div>
+									<label htmlFor="email" className="block text-sm font-medium text-gray-700">
+									Email address
+									</label>
+									<div className="mt-1">
+									<input
+										type="email"
+										autoComplete="email"
+										required
+										defaultValue={email}
+										onChange={(e)=>{setEmail(e.target.value)}}
+										className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+									/>
+									</div>
+								</div>
+
+
+								
+								</div>
+							</div>
+							</div>
+						</div>
+						<div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+							<button
+								type="button"
+								className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 sm:ml-3 sm:w-auto sm:text-sm"
+								onClick={() => {
+									const { data, error } = supabase.auth.api.resetPasswordForEmail(email);
+									setOpen(false);
+								}}
+							>
+								Get Reset Link
+							</button>
+							<button
+								type="button"
+								className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+								onClick={() => setOpen(false)}
+								ref={cancelButtonRef}
+							>
+								Cancel
+							</button>
+						</div>
+						</div>
+					</Transition.Child>
+					</div>
+				</Dialog>
+				</Transition.Root>
 		</div>
 	)
 }
