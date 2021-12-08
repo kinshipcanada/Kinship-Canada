38dbc01ece53ced5bc1f0f772cccfdd6d7e09be4
hobbleabbas <shakeel@atlasfinance.org>
2021-12-08 17:16:29 -0500

    changed prices

--- pages/campaigns/orphans.js
+++ pages/campaigns/orphans.js
@@ -11,13 +11,13 @@ export default function Orphans() {
                 <h1 className = "font-medium text-2xl mb-3">Help Support An Orphan</h1>
                 <p className = "font-regular text-lg">Kinship Canada is raising for a campaign to try and sponser 12 children this month, in the name of Sayyida Fatema (A.S.). Please contribute as much as you can.</p>
                 <div className = "flex justify-center mt-4">
-                    <Link href='https://buy.stripe.com/3cseVgeV39BWcqQ146'>
+                    <Link href='https://buy.stripe.com/00geVg5ktcO8bmM6or'>
                         <a className="inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-blue-600 hover:bg-blue-700">
                             Donate $150 Per Month
                             <ExternalLinkIcon className = "ml-2 w-5 h-5"/>
                         </a>
                     </Link>
-                    <Link href='https://buy.stripe.com/bIY9AW14d8xS0I89AB'>
+                    <Link href='https://buy.stripe.com/5kAfZk8wF5lG2QgdQU'>
                         <a className="ml-2 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-blue-600 hover:bg-blue-700">
                             Donate $1800 For A Year
                             <ExternalLinkIcon className = "ml-2 w-5 h-5"/>
