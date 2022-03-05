1fcbe6754f5f82226c7f7fbee6e561d52c5a709c
hobbleabbas <shakeel@atlasfinance.org>
2022-03-05 18:43:07 -0500

    redirects

--- next.config.js
+++ next.config.js
@@ -1,3 +1,22 @@
 module.exports = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  async redirects() {
+    return [
+      {
+        source: '/ramadan',
+        destination: '/campaigns/ramadhan',
+        permanent: true,
+      },
+      {
+        source: '/ramadhan',
+        destination: '/campaigns/ramadhan',
+        permanent: true,
+      },
+      {
+        source: '/campaigns/ramadan',
+        destination: '/campaigns/ramadhan',
+        permanent: true,
+      },
+    ]
+  },
 }
