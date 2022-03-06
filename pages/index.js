5b5d9f20c8e35bffd37dc85b3c3f3c4d59e1db79
hobbleabbas <74548347+hobbleabbas@users.noreply.github.com>
2022-03-05 19:17:53 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -415,7 +415,7 @@ export const Banner = () => {
             </p>
           </div>
           <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
-            <Link href = "/campaigns/orphans">
+            <Link href = "/campaigns/ramadhan">
               <a
                 className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-blue-600 bg-white hover:bg-blue-50"
               >
